f17dd8084ff1ce5909a9d6d9efb7fc036b2a22e6
alvinsaputra123 <alvin.saputra20162016@gmail.com>
2024-09-08 22:35:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, maxime at, possimus atque autem quibusdam nobis dicta ullam libero numquam eum id. Nesciunt, obcaecati doloremque quos eius eveniet ad fuga.</p>
+    <ul>
+        <li>coba</li>
+    </ul>
 </body>
 </html>
